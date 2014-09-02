5f48082cddeeed5796f23ea3f7f4e75e1e354c7e
Romain <r.monnard@gmail.com>
2014-09-02 17:38:03 +0200

    Base calendar for the module

--- staff_management/static/src/js/staff_calendar.js
+++ staff_management/static/src/js/staff_calendar.js
@@ -1,9 +1,109 @@
 openerp_staff_management_calendar = function(instance) {
+	var _t = instance.web._t;
+
+	function get_fc_defaultOptions() {
+        shortTimeformat = Date.CultureInfo.formatPatterns.shortTime;
+        return {
+            weekNumberTitle: _t("W"),
+            allDayText: _t("All day"),
+            buttonText : {
+                today:    _t("Today"),
+                month:    _t("Month"),
+                week:     _t("Week"),
+                day:      _t("Day")
+            },
+            monthNames: Date.CultureInfo.monthNames,
+            monthNamesShort: Date.CultureInfo.abbreviatedMonthNames,
+            dayNames: Date.CultureInfo.dayNames,
+            dayNamesShort: Date.CultureInfo.abbreviatedDayNames,
+            firstDay: Date.CultureInfo.firstDayOfWeek,
+            weekNumbers: false,
+            axisFormat : shortTimeformat.replace(/:mm/,'(:mm)'),
+            timeFormat : {
+               // for agendaWeek and agendaDay               
+               agenda: shortTimeformat + '{ - ' + shortTimeformat + '}', // 5:00 - 6:30
+                // for all other views
+                '': shortTimeformat.replace(/:mm/,'(:mm)')  // 7pm
+            },
+            weekMode : 'liquid',
+            aspectRatio: 1.8,
+            snapMinutes: 15,
+        };
+    }
 
 	instance.staff_management.Calendar = instance.web_calendar.CalendarView.extend({
 		init:function(){
 			this._super.apply(this,arguments);
 		},
+		
+		// No slidebar
+		init_calendar: function() {
+            var self = this;
+            
+            self.$calendar.fullCalendar(self.get_fc_init_options());
+            
+            return $.when();
+        },
+		
+		get_fc_init_options: function () {
+            //Documentation here : http://arshaw.com/fullcalendar/docs/
+            var self = this;
+            return  $.extend({}, get_fc_defaultOptions(), {
+                
+                defaultView: "month",
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: '' // 'month' Nothing, only one view
+                },
+                selectable: !this.options.read_only_mode && this.create_right,
+                selectHelper: true,
+                editable: !this.options.read_only_mode,
+                droppable: true,
+
+                // callbacks
+
+                eventDrop: function (event, _day_delta, _minute_delta, _all_day, _revertFunc) {
+                    var data = self.get_event_data(event);
+                    self.proxy('update_record')(event._id, data); // we don't revert the event, but update it.
+                },
+                eventResize: function (event, _day_delta, _minute_delta, _revertFunc) {
+                    var data = self.get_event_data(event);
+                    self.proxy('update_record')(event._id, data);
+                },
+                eventRender: function (event, element, view) {
+                    element.find('.fc-event-title').html(event.title);
+                },
+                eventAfterRender: function (event, element, view) {
+                    if ((view.name !== 'month') && (((event.end-event.start)/60000)<=30)) {
+                        //if duration is too small, we see the html code of img
+                        var current_title = $(element.find('.fc-event-time')).text();
+                        var new_title = current_title.substr(0,current_title.indexOf("<img")>0?current_title.indexOf("<img"):current_title.length);
+                        element.find('.fc-event-time').html(new_title);
+                    }
+                },
+                eventClick: function (event) { self.open_event(event._id,event.title); },
+                select: function (start_date, end_date, all_day, _js_event, _view) {
+                    var data_template = self.get_event_data({
+                        start: start_date,
+                        end: end_date,
+                        allDay: all_day,
+                    });
+                    self.open_quick_create(data_template);
+
+                },
+
+                unselectAuto: false,
+				height: $('.oe_view_manager_body').height() - 5,
+	            handleWindowResize: true,
+	            windowResize: function(view) {
+			        self.$calendar.fullCalendar('option', 'height', $('.oe_view_manager_body').height() - 5);
+			    }
+
+            });
+        },
+		
+		
 	});
 
 };
