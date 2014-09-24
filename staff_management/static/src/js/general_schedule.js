acd19648b6f55662dd61b87934ec0ff1922401d6
Romain <r.monnard@gmail.com>
2014-09-23 21:00:19 -0700

    Preparation for timelines views

--- staff_management/static/src/js/general_schedule.js
+++ staff_management/static/src/js/general_schedule.js
@@ -4,6 +4,136 @@ openerp_staff_management_general_schedule = function(instance) {
 	
 	instance.staff_management.GeneralScheduler = instance.staff_management.Timeline.extend({
 		
+		init:function(parent, dataset, view_id, options){
+			this._super.apply(this, arguments);
+			
+			this.set_interval('day', 1);
+			this.set_nbrOfHeaderLines(2);
+			
+			var curr = new Date; // get current date
+			var first = curr.getDate() - curr.getDay(); // First day is the day of the month - the day of the week
+			var last = first + 6; // last day is the first day + 6
+			
+			var firstday = new Date(curr.setDate(first));
+			var lastday = new Date(curr.setDate(last));
+			
+			this.set_range_dates(firstday, lastday);
+						
+			
+		},
+		
+		view_loading: function (fv) {
+		
+			var curr = new Date;
+			var first = curr.getDate() - curr.getDay();
+			var one = new Date(curr.setDate(first));
+			
+			var two = this.getNextDate(one);
+			var three = this.getNextDate(two);
+			var four = this.getNextDate(three);
+			var five = this.getNextDate(four);
+			var six = this.getNextDate(five);
+			var seven = this.getNextDate(six);
+			
+		
+			var datas = [
+				{
+					'html': $('<span>').text('User1'),
+					'lineID': 1,
+					'cells': [
+						{
+							'html': $('<span>').text(1),
+							'tooltip': $('<span>').text(1),
+							'date': one,
+						},
+						{
+							'html': $('<span>').text(2),
+							'tooltip': $('<span>').text(2),
+							'date': two,
+						},
+						{
+							'html': $('<span>').text(3),
+							'tooltip': $('<span>').text(3),
+							'date': three,
+						},
+						{
+							'html': $('<span>').text(4),
+							'tooltip': $('<span>').text(4),
+							'date': four,
+						},
+						{
+							'html': $('<span>').text(5),
+							'tooltip': $('<span>').text(5),
+							'date': five,
+						},
+						{
+							'html': $('<span>').text(6),
+							'tooltip': $('<span>').text(6),
+							'date': six,
+						},
+						{
+							'html': $('<span>').text(7),
+							'tooltip': $('<span>').text(7),
+							'date': seven,
+						},
+					]
+				},
+				{
+					'html': $('<span>').text('User2'),
+					'lineID': 2,
+					'cells': [
+						{
+							'html': $('<span>').text(1),
+							'tooltip': $('<span>').text(1),
+							'date': one,
+						},
+						{
+							'html': $('<span>').text(2),
+							'tooltip': $('<span>').text(2),
+							'date': two,
+						},
+						{
+							'html': $('<span>').text(6),
+							'tooltip': $('<span>').text(6),
+							'date': six,
+						},
+						{
+							'html': $('<span>').text(7),
+							'tooltip': $('<span>').text(7),
+							'date': seven,
+						},
+					],
+				},
+				{
+					'html': $('<span>').text('User3'),
+					'lineID': 3,
+					'cells': [
+						{
+							'html': $('<span>').text(4),
+							'tooltip': $('<span>').text(4),
+							'date': four,
+						},
+						{
+							'html': $('<span>').text(7),
+							'tooltip': $('<span>').text(7),
+							'date': seven,
+						},
+					],
+				},
+			];
+		
+			this.update_datas(datas);
+		},
+		
+		
+		renderCell: function(cellDataList){
+			if(cellDataList.length == 1){
+				return cellDataList[0]['html'];
+			}
+			return $('<span>');
+		},
+		
+		
 	});
 
 }
--- staff_management/static/src/js/timeline_base.js
+++ staff_management/static/src/js/timeline_base.js
@@ -13,6 +13,223 @@ openerp_staff_management_timeline_base = function(instance) {
 			this._super();
 		},
 		
+		/*
+			interval_mode: day | month | year
+			interval_nbr: nbr of day, month or year to add by step
+		*/
+		set_interval: function(interval_mode, interval_nbr){
+			this.interval_mode = interval_mode;
+			this.interval_nbr = interval_nbr;
+		},
+		
+		/*
+			data: {
+				date: {
+					'html': jqueryDom,
+					'tooltip': jqueryDom,
+					'line_id': id,
+				},
+				...
+			}
+		*/
+		update_datas: function(datas){
+			this.datas = datas;
+			this.render_timeline();
+		},
+		
+		
+		update_lines: function(lineDatas){
+			this.set_line(lineDatas);
+			this.render_timeline();
+		},
+		
+		/*
+			lineDatas: [
+				{
+					'html': jqueryDom,
+					'lineID': id,
+					'cells': [
+						{ // lineID
+							'html': jqueryDom,
+							'tooltip': jqueryDom,
+							'date': date,
+						},
+					],
+				},
+				...
+			]
+		*/
+		set_lines: function(lineDatas){
+			this.lineDatas = lineDatas;
+		},
+		
+		
+		set_nbrOfHeaderLines: function(nbrOfHeaderLines){
+			this.nbrOfHeaderLines = nbrOfHeaderLines;
+		},
+		
+		headerCellContent: function(lineID, date){
+			return $('<span>').text('-');
+		},
+		
+		renderCell: function(cellDataList){
+			return $('<span>');
+		},
+		
+		set_range_dates: function(date_start, date_stop) {
+			this.range_start = new Date(date_start);
+			this.range_stop = new Date(date_stop);
+		},
+		
+		update_range_dates: function(date_start, date_stop) {
+			this.set_range_dates(date_start, date_stop);
+			this.render_timeline();
+		},
+		
+		getNextDate: function(date){
+			var d = new Date(date.getTime());
+			if(this.interval_mode == 'day'){
+				d.setDate(d.getDate() + this.interval_nbr);
+			}
+			else if(this.interval_mode == 'month'){
+				d.setMonth(d.getMonth() + this.interval_nbr);
+			}
+			else if(this.interval_mode == 'year'){
+				d.setFullYear(d.getFullYear() + this.interval_nbr);
+			}
+			return d;
+		},
+		
+		isSameDate: function(d1, d2){
+			var format = "yyyy-MM-dd";
+			if(this.interval_mode == 'month'){
+				format = "yyyy-MM";
+			}
+			else if(this.interval_mode == 'year'){
+				format = "yyyy";
+			}
+			var d1_str = $.fullCalendar.formatDate(d1, format);
+			var d2_str = $.fullCalendar.formatDate(d2, format);
+			return (d1_str == d2_str);
+		},
+		
+		render_timeline: function(){
+			
+			var table = $('<table>').attr('width', '100%');
+			var thead = $('<thead>');
+			var tbody = $('<tbody>');
+			var tfoot = $('<tfoot>');
+			
+			thead = this.header_rendering(thead);
+			tbody = this.body_rendering(tbody);
+						
+			table.append(thead);
+			table.append(tbody);
+			table.append(tfoot);
+
+			$('.stimeline_table').empty();
+			$('.stimeline_table').append(table);
+			
+			this.final_table_rendering(table);
+		},
+		
+		header_rendering: function(thead){
+			
+			for(var lineID=1 ; lineID<=this.nbrOfHeaderLines ; lineID ++){
+				
+				var tr = $('<tr>');
+				var th = $('<th>').addClass('stimeline_leftcol');
+				th.text('Titre');
+				tr.append(th);
+				
+				for(var cdate=this.range_start ; cdate<=this.range_stop ; cdate=this.getNextDate(cdate)){
+					var th = $('<th>');
+					
+					th.append(this.headerCellContent(lineID, cdate));
+					
+					tr.append(th);
+				}
+				
+				thead.append(tr);
+				
+			}
+			return thead;
+		},
+		
+		body_rendering: function(tbody){
+			
+			for(var i=0 ; i<this.datas.length ; i++){
+				var data = this.datas[i];
+				
+				var tr = $('<tr>');
+				var th = $('<th>').addClass('stimeline_leftcol');
+				th.append(data['html']);
+				tr.append(th);
+				
+				for(var cdate=this.range_start ; cdate<=this.range_stop ; cdate=this.getNextDate(cdate)){
+					var td = $('<td>');
+					
+					var cellDataList = [];
+					for(var j=0 ; j<data['cells'].length ; j++){
+						if(this.isSameDate(data['cells'][j]['date'], cdate)){
+							cellDataList.push(data['cells'][j]);
+						}
+					}
+					
+					td.append(this.renderCell(cellDataList));
+					
+					
+					tr.append(td);
+				}
+				
+				tbody.append(tr);
+				
+			}
+			return tbody;
+		},
+		
+		
+		final_table_rendering: function(table){
+		
+			var viewHeight = $('.openerp_webclient_container').height() - $('.announcement_bar').height() - $('.oe_topbar').height() - $('.oe_view_manager_header').height();
+			
+			
+			var tableHeight = viewHeight - $('.stimeline_header').height();
+			
+			var tbodyHeight = tableHeight - $('.stimeline_table thead').height() - $('.stimeline_table tfoot').height() - 10;
+			
+			width = $(window).width() - $('.oe_leftbar').width() - 1;
+			$('.salary_timeline').css({
+				'width': width,
+			});
+		
+			table.dataTable({
+				"searching": false,
+				"info": false,
+				"paging":   false,
+				"order": [[ 0, "asc" ]],
+				"scrollY": tbodyHeight,
+				"scrollX": true,
+				"language": {
+					"emptyTable":     _t("No data available"),
+				}
+			});	
+			
+			
+			var realTbodyHeight = $('.stimeline_table tbody').height();
+			if(realTbodyHeight < tbodyHeight){
+				nbrLines = (this.datas.length == 0) ? 1 : this.datas.length;
+				var tr_height = tbodyHeight / nbrLines - 1;
+				$('.stimeline_table tbody tr').each(function(i, e){
+					$(e).height(tr_height);
+				});	
+			}
+			
+		},
+		
+		
+		
+		
 	});
 
 };
